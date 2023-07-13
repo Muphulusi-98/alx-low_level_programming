cd4788912ab246e69fb2024f326f2beabf64f30a
Muphulusi-98 <muphulusi.matshivha@gmail.com>
2023-07-13 08:12:52 -0700

    task2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,84 +1,25 @@
-#include <stdlib.h>
-
 #include "main.h"
 
-
 /**
-
- * *_memset - fills memory with a constant byte
-
- * @s: memory area to be filled
-
- * @b: char to copy
-
- * @n: number of times to copy b
-
+ * _calloc - allocates memory for an array, initialized to 0
+ * @nmemb: number of elements
+ * @size: byte size of each element
  *
-
- * Return: pointer to the memory area s
-
+ * Return: void pointer to array space
  */
-
-char *_memset(char *s, char b, unsigned int n)
-
-{
-
-        unsigned int i;
-
-
-        for (i = 0; i < n; i++)
-
-        {
-
-                s[i] = b;
-
-        }
-
-
-        return (s);
-
-}
-
-
-/**
-
- * *_calloc - allocates memory for an array
-
- * @nmemb: number of elements in the array
-
- * @size: size of each element
-
- *
-
- * Return: pointer to allocated memory
-
- */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
-
 {
+	char *p;
 
-        char *ptr;
-
-
-        if (nmemb == 0 || size == 0)
-
-                return (NULL);
-
-
-        ptr = malloc(size * nmemb);
-
-
-        if (ptr == NULL)
-
-                return (NULL);
-
-
-        _memset(ptr, 0, nmemb * size);
-
-
-        return (ptr);
-
+	if (!nmemb || !size)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (!p)
+		return (NULL);
+	nmemb *= size;
+	while (nmemb--)
+		p[nmemb] = 0;
+	return (p);
 }
 
 
