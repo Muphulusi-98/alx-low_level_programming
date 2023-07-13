e1788a82d39315832f7a97eb350788a0fc588557
Muphulusi-98 <muphulusi.matshivha@gmail.com>
2023-07-13 09:48:19 -0700

    task5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,98 +1,39 @@
+#include "main.h"
 #include <stdlib.h>
 
-#include "main.h"
-
-
 /**
-
- * *_realloc - reallocates a memory block using malloc and free
-
- * @ptr: pointer to the memory previsouly allocated by malloc
-
- * @old_size: size of the allocated memory for ptr
-
- * @new_size: new size of the new memory block
-
- *
-
- * Return: pointer to the newly allocated memory block
-
+ * _realloc - Main Entry
+ * @ptr: input
+ * @old_size: input
+ * @new_size: input
+ * Return: 0
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-
 {
+	char *p;
+	unsigned int i, max = new_size;
+	char *oldp = ptr;
 
-        char *ptr1;
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		return (p);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (new_size == old_size)
+		return (ptr);
 
-        char *old_ptr;
-
-        unsigned int i;
-
-
-        if (new_size == old_size)
-
-                return (ptr);
-
-
-        if (new_size == 0 && ptr)
-
-        {
-
-                free(ptr);
-
-                return (NULL);
-
-        }
-
-
-        if (!ptr)
-
-                return (malloc(new_size));
-
-
-        ptr1 = malloc(new_size);
-
-        if (!ptr1)
-
-                return (NULL);
-
-
-        old_ptr = ptr;
-
-
-        if (new_size < old_size)
-
-        {
-
-                for (i = 0; i < new_size; i++)
-
-                        ptr1[i] = old_ptr[i];
-
-        }
-
-
-        if (new_size > old_size)
-
-        {
-
-                for (i = 0; i < old_size; i++)
-
-                        ptr1[i] = old_ptr[i];
-
-        }
-
-
-        free(ptr);
-
-        return (ptr1);
-
+	p = malloc(new_size);
+	if (p == NULL)
+		return (NULL);
+	if (new_size > old_size)
+		max = old_size;
+	for (i = 0; i < max; i++)
+		p[i] = oldp[i];
+	free(ptr);
+	return (p);
 }
-
-
-
-
-
-
-
-
