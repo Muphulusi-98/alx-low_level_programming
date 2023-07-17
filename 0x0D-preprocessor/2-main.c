54c4bf919786d18d3de604466df9c8cb7878328c
Muphulusi-98 <muphulusi.matshivha@gmail.com>
2023-07-17 06:40:18 -0700

    task2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,24 @@
-#include &lt;stdio.h&gt;
+#include <stdio.h>
+
+
 /**
-* main - prints the name of the file
-*
-* Return: Always 0 (Success)
-*/
+
+ * main - prints the name of the file
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
 int main(void)
+
 {
-printf(&quot;%s\n&quot;, __FILE__);
-return (0);
+
+        printf("%s\n", __FILE__);
+
+        return (0);
+
 }
+
+
